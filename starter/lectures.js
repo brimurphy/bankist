2196d356aaf8e1cf1a5a8c9d44d08a72d51f6f5e
Brian <blpmurphy@gmail.com>
2021-09-03 11:21:19 +0100

    Lesson: 162 - Creating and filling arrays

--- starter/lectures.js
+++ starter/lectures.js
@@ -390,7 +390,7 @@ const overalBalanceChain2 = accountsNew
 
 console.log(overalBalanceChain2);
 */
-
+/*
 // Sort method (mutates original array)
 
 // Strings
@@ -419,3 +419,41 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty array + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5)); This doesn't work
+
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+// const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  // We could also spread the movements into an array
+  // but we would have to map the result to get the value
+  // const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  // console.log(movementsUI2);
+});
