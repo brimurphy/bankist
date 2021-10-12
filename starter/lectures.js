6cc5afa4184e2bed56028407b9cdadd064ae02ab
Brian <blpmurphy@gmail.com>
2021-10-12 21:10:27 +0100

    Fix decimal places error in app and Math and Rounding lession

--- starter/lectures.js
+++ starter/lectures.js
@@ -622,7 +622,7 @@ const dogsRecFoodSorted = dogs
   .sort((a, b) => a.recommendedFood - b.recommendedFood);
 console.log(dogsRecFoodSorted);
 */
-
+/*
 //  Converting and Checking Numbers
 
 console.log(23 === 23.0);
@@ -657,3 +657,47 @@ console.log(Number.isFinite(20)); // True
 console.log(Number.isFinite('20')); // false (it's a string not a number)
 console.log(Number.isFinite(+'20X')); // false
 console.log(Number.isFinite(23 / 0)); // false (infinite is not finite)
+*/
+
+// Math and Rounding
+
+console.log(Math.sqrt(25)); // square root
+console.log(25 ** (1 / 2)); // square root
+console.log(8 ** (1 / 3)); // cubic root
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2)); // Will convert
+console.log(Math.max(5, 18, '23px', 11, 2)); // Won't parse
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.random() * 6);
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+// 0...1 => (max - min) -> min...max
+console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.8));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.8));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.8'));
+
+console.log(Math.trunc(-23.8));
+console.log(Math.floor(-23.8));
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // returns a string
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2)); // returns a number
